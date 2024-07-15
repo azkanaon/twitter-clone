91cd4d5b9a19ed5c61b046b9e77e09812fa78d58
azka <azkaatqiya4@gmail.com>
2024-07-15 17:50:34 +0700

    1- auth controlled added

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,21 @@ import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json()); // to parse req.body
+app.use(express.urlencoded({ extended: true })); // to parse form data
+
+app.use(cookieParser()); // to parse cookies
 
 app.use("/api/auth", authRoutes);
 
-app.listen(8000, () => {
-  console.log("server is running in 8000");
+app.listen(PORT, () => {
+  console.log("server is running in ", PORT);
   connectMongoDB();
 });
